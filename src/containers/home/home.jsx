027df0fda9c6c52fb98fcdff8b371a92bedd2b8c
ahmadthoyib <thoyibghofur16@gmail.com>
2019-04-29 14:35:00 +0700

    first commit

---
+++ src/containers/home/home.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Navbar from "../../components/navbar";
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar name="GO-FOOD" />
+      </div>
+    );
+  }
+}
+
+export default Home;
